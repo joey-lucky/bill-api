966a7a73555dcc130ff6d369d7d6ebc3d39c74bc
273179370@qq.com <273179370@qq.com>
2020-07-11 12:22:21 +0800

    feat:日志输出

--- src/service/logger/logger.service.ts
+++ src/service/logger/logger.service.ts
@@ -1,16 +1,16 @@
 import {Injectable, Logger as NestLogger} from "@nestjs/common";
 import {LoggerService as NestService} from "@nestjs/common/services/logger.service";
-import {Logger} from "typeorm/logger/Logger";
-import {QueryRunner} from "typeorm";
+import {QueryRunner,Logger as TypeOrmLogger} from "typeorm";
+import {configure, getLogger, Logger} from "log4js";
 import moment = require("moment");
 
 class CommonLogger implements NestService {
-    private readonly logger: NestLogger;
+    private readonly logger: Logger;
     private readonly loggerName: string;
 
     constructor(loggerName: string) {
         this.loggerName = loggerName;
-        this.logger = new NestLogger(loggerName);
+        this.logger = getLogger(loggerName);
     }
 
     error(message: any, trace?: string, context?: string): any {
@@ -18,7 +18,7 @@ class CommonLogger implements NestService {
     }
 
     log(message: any, context?: string): any {
-        return this.logger.log(`${message}`, context);
+        return this.logger.info(`${message}`, context);
     }
 
     warn(message: any, context?: string): any {
@@ -30,50 +30,50 @@ class CommonLogger implements NestService {
     }
 
     verbose(message: any, context?: string): any {
-        return this.logger.verbose(`${message}`, context);
+        return this.logger.trace(`${message}`, context);
     }
 }
 
-class DbLogger implements Logger {
+class DbLogger implements TypeOrmLogger {
     logService = new CommonLogger("database");
 
     log(level: "log" | "info" | "warn", message: any, queryRunner?: QueryRunner): any {
         if (level === "warn") {
-            this.logService.warn(message);
+            this.logService.warn(this.toSimpleSql(message));
         } else if (level === "info") {
-            this.logService.verbose(message);
+            this.logService.verbose(this.toSimpleSql(message));
         } else {
-            this.logService.log(message);
+            this.logService.log(this.toSimpleSql(message));
         }
     }
 
     logMigration(message: string, queryRunner?: QueryRunner): any {
+        this.logService.verbose(this.toSimpleSql(message));
     }
 
     logQuery(query: string, parameters?: any[], queryRunner?: QueryRunner): any {
-        this.logService.verbose(this.toSimpleSql(query,parameters));
+        this.logService.verbose(this.toSimpleSql(query, parameters));
     }
 
     logQueryError(error: string, query: string, parameters?: any[], queryRunner?: QueryRunner): any {
-        this.logService.error(this.toSimpleSql(query,parameters), error);
+        this.logService.error(this.toSimpleSql(query, parameters), error);
     }
 
     logQuerySlow(time: number, query: string, parameters?: any[], queryRunner?: QueryRunner): any {
-        this.logService.verbose(this.toSimpleSql(query,parameters) + `\t(${time}ms)`);
+        this.logService.verbose(this.toSimpleSql(query, parameters) + `\t(${time}ms)`);
     }
 
     logSchemaBuild(message: string, queryRunner?: QueryRunner): any {
-
+        this.logService.verbose(this.toSimpleSql(message));
     }
 
     toSimpleSql(sql: string, parameters?: any[]) {
-        sql = sql.replace(/(\n|\t)/g, "   ");
-        sql = sql.replace(/[\s]{2}/g, "");
+        sql = sql.replace(/[\n\t]/g, "  ");
+        sql = sql.replace(/[\s]{2,9999}/g, " ");
         if (sql.startsWith(" ")) {
             sql = sql.replace(" ", "");
         }
-
-        let parameterStr ="";
+        let parameterStr = "";
         if (parameters) {
             parameters = parameters.map(item => {
                 if (item instanceof Date) {
@@ -91,7 +91,60 @@ class DbLogger implements Logger {
 
 @Injectable()
 export class LoggerService {
-    public readonly scheduleLogger = new CommonLogger("schedule")
-    public readonly requestLogger = new CommonLogger("request")
-    public readonly dbLogger = new DbLogger()
+    public readonly scheduleLogger;
+    public readonly requestLogger;
+    public readonly dbLogger;
+
+    constructor() {
+        configure({
+            pm2: true,
+            appenders: {
+                console: {
+                    type: 'console',
+                    layout: {
+                        type: "colored"
+                    }
+                },
+                databaseFile: {
+                    type: 'DateFile',
+                    filename: 'logs/database',
+                    pattern: '.yyyyMMdd.log',
+                    alwaysIncludePattern: true,
+                },
+                requestFile: {
+                    type: 'DateFile',
+                    filename: 'logs/request',
+                    pattern: '.yyyyMMdd.log',
+                    alwaysIncludePattern: true,
+                },
+                scheduleFile:{
+                    type: 'DateFile',
+                    filename: 'logs/schedule',
+                    pattern: '.yyyyMMdd.log',
+                    alwaysIncludePattern: true,
+                },
+            },
+            categories: {
+                default: {
+                    appenders: ['console'],
+                    level: 'all'
+                },
+                schedule:{
+                    appenders: ['console', "scheduleFile"],
+                    level: 'all'
+                },
+                request:{
+                    appenders: ['console', "requestFile"],
+                    level: 'all'
+                }        ,
+                database:{
+                    appenders: ['console', "databaseFile"],
+                    level: 'all'
+                }
+            }
+        });
+        this.scheduleLogger = new CommonLogger("schedule");
+        this.requestLogger = new CommonLogger("request")
+        this.dbLogger = new DbLogger();
+    }
 }
--- test.js
+++ test.js
@@ -1,4 +1,23 @@
-const moment = require("moment");
+function toSimpleSql(sql, parameters) {
+    sql = sql.replace(/[^_@\.\-\:0-9a-zA-Z]/g, " ");
+    sql = sql.replace(/[\s]{2,9999}/g, " ");
+    sql = sql.replace(/\s\.\s/g, ".");
+    if (sql.startsWith(" ")) {
+        sql = sql.replace(" ", "");
+    }
+    let parameterStr = "";
+    if (parameters) {
+        parameters = parameters.map(item => {
+            if (item instanceof Date) {
+                return moment(item).format("YYYY-MM-DD HH:mm:ss");
+            } else {
+                return item;
+            }
+        });
+        parameterStr = parameters.toString();
+    }
+    return sql + ` [${parameterStr}]`;
+}
 
-console.log(moment().diff(moment(),"day"))
-console.log(moment("2020-05-01").add(1,"day").diff(moment("2020-05-01"),"day"))
+let a = "CREATE TABLE bill_dev . typeorm_metadata type varchar 255 NOT NULL database ";
+console.log(toSimpleSql(a))
